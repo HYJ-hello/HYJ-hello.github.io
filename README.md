2e2b6252592f9bfcc39a65c44f62e0f1c1003c89
HYJ-hello <3226268396@qq.com>
2026-07-10 03:27:29 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [8](https://HYJ-hello.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 11180 
-### :alarm_clock: 2026-07-10 11:23:04 
+### :alarm_clock: 2026-07-10 11:27:29 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/test-screen.js
+++
@@ -1,9 +0,0 @@
-// 当页面加载完成后，在控制台和页面上显示信息
-window.onload = function() {
-    console.log('欢迎来到我的Gmeek博客！');
-    // 在页面顶部显示一条信息
-    const msg = document.createElement('div');
-    msg.innerHTML = '🎉 欢迎光临！';
-    msg.style.cssText = 'background: #f0f0f0; padding: 10px; text-align: center;';
-    document.body.prepend(msg);
-};
